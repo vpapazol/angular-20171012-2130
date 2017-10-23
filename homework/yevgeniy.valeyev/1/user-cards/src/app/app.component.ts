c7d3a6e073fa8a7b012ccdafebf3fd1a2fe01c4a
Yevgeniy Valeyev <webmaster.vea@gmail.com>
2017-10-23 12:25:08 +0200

    Added HW 1

--- homework/yevgeniy.valeyev/1/user-cards/src/app/app.component.ts
+++ homework/yevgeniy.valeyev/1/user-cards/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { USERS } from './users';
+import { User, UserSelectionData } from './interfaces';
+import * as utils from './utils';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  users: User[] = USERS;
+  selectedUserIds: number[] = [];
+
+  deleteUser (user: UserSelectionData) {
+    const index = this.users.findIndex(({id}) => user.id === id);
+    if (index >= 0) {
+      this.users = utils.removeItemFromList(this.users, index);;
+    }
+  }
+
+  isUserInSelection ({id}: UserSelectionData) {
+    return this.selectedUserIds.indexOf(id) >= 0;
+  }
+
+  addUserToSelection (user: UserSelectionData) {
+    if (!this.isUserInSelection(user)) {
+      this.selectedUserIds = [...this.selectedUserIds, user.id]
+    }
+  }
+
+  removeUserFromSelection (user: UserSelectionData) {
+    if (this.isUserInSelection(user)) {
+      const index = this.selectedUserIds.indexOf(user.id)
+      this.selectedUserIds = utils.removeItemFromList(this.selectedUserIds, index);
+    }
+  }
 }
---
+++ homework/yevgeniy.valeyev/1/user-cards/src/app/users.ts
@@ -0,0 +1,14 @@
+import { User } from './interfaces';
+
+export const USERS: User[] = [
+  {
+    id: 1,
+    firstName: 'Alex',
+    lastName: 'Rogers'
+  },
+  {
+    id: 2,
+    firstName: 'Mike',
+    lastName: 'Smith'
+  }
+]
