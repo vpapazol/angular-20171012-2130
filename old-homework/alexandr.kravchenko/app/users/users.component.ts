d9ba25673072b90fd5869b06b6be69686c21d94a
Alex Kravchenko <krav4enko@mail.ru>
2017-10-28 16:50:53 +0300

    homework 2

---
+++ old-homework/alexandr.kravchenko/app/users/users.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  @Input() user;
+  @Output() userDelete: EventEmitter<any> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  public deleteUser(id) {
+    this.userDelete.emit(id);
+  }
+}
