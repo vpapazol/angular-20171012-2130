c6df9919420d69b1caecc953e9ade072c186e747
dimitriy <dimitriy@anyforsoft.com>
2017-10-29 15:48:49 +0200

    Added homework 3

---
+++ homework/dimitriy.ryzhakov/src/app/box/box.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-box',
+  templateUrl: './box.component.html',
+  styleUrls: ['./box.component.css']
+})
+export class BoxComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+
+}
